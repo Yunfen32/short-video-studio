67d374b72473cc1179a48316649ad0d183aba3f5
缘分啊朋友 <14860748+fate-my-friend@user.noreply.gitee.com>
2026-07-10 09:48:42 +0800

    Add Sites worker build

---
+++ worker/index.js
@@ -0,0 +1,27 @@
+const NOT_FOUND = new Response("Not found", { status: 404 });
+
+function assetRequest(request, pathname) {
+  const url = new URL(request.url);
+  url.pathname = pathname;
+  url.search = "";
+  return new Request(url, request);
+}
+
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    const pathname = url.pathname === "/" ? "/index.html" : url.pathname;
+
+    if (!env?.ASSETS?.fetch) {
+      return NOT_FOUND;
+    }
+
+    let response = await env.ASSETS.fetch(assetRequest(request, pathname));
+
+    if (response.status === 404 && !pathname.includes(".")) {
+      response = await env.ASSETS.fetch(assetRequest(request, "/index.html"));
+    }
+
+    return response;
+  },
+};
